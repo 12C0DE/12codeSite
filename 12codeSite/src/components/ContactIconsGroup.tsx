daee4b7e45c06f658a17788e87a43b4243ef70ea
Ruben Hernandez <Rubenhnt@gmail.com>
2026-01-12 17:26:50 +0000

    finished contact icons

--- 12codeSite/src/components/ContactIconsGroup.tsx
+++ 12codeSite/src/components/ContactIconsGroup.tsx
@@ -1,21 +1,20 @@
 import { Github, Linkedin, Send } from "lucide-react";
 import { Tooltip } from "react-tooltip";
 
-// TODO: Add Hover effects to icons
 export const ContaactIconsGroup = () => {
   return (
     <div className="flex flex-col gap-4 fixed bottom-8 right-4">
       <a href="mailto:12codeHz@gmail.com">
-        <Send size={32} data-tooltip-id="emailTip" />
+        <Send size={32} data-tooltip-id="emailTip" className="hover:opacity-70"/>
       </a>
       <a href="https://github.com/12C0DE" target="_blank">
-        <Github size={32} data-tooltip-id="ghTip" />
+        <Github size={32} data-tooltip-id="ghTip" className="hover:text-[var(--github-color)] transition-colors"/>
       </a>
       <a
         href="https://www.linkedin.com/in/ruben-hernandez-7a7b5128a/"
         target="_blank"
       >
-        <Linkedin size={32} strokeWidth={1.5} data-tooltip-id="linkedInTip" />
+        <Linkedin size={32} strokeWidth={1.5} data-tooltip-id="linkedInTip" className="hover:text-[var(--linkedIn-color)] transition-colors"/>
       </a>
       <Tooltip
         id="emailTip"
